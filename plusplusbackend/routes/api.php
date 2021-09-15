fc2b8ab8ce2fb67adb4047c2d18eb7bd66fbfd37
ZaIhany22 <rotsy96.razanakoto@gmail.com>
2021-09-15 12:38:04 +0300

    Backend miaraka am base de donne (login et register)

--- plusplusbackend/routes/api.php
+++ plusplusbackend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::get('/test',function(){
     return ["test"=>"mety"];
+});*/
+
+Route::post('login',[UserController::class,'login']);
+
+Route::post('register',[UserController::class,'register']);
+
+Route::group(['middleware'=>'auth:sanctum'],function(){
+    Route::post('logout',[UserController::class,'logout']);
 });
