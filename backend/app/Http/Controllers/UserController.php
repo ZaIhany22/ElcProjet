683e6cab12f2543400c34b7ad4cbe79ddf5b49f5
ZaIhany22 <rotsy96.razanakoto@gmail.com>
2021-09-27 09:32:54 +0300

    version final

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -15,13 +15,68 @@ class UserController extends Controller
     public function login(Request $request)  {
         $fields=$request->validate([
             'email'=>'required|string',
-            'password'=>'required|string'
+            'password'=>'required|string',
+            'role'=>'required'
         ]);
 
         $user=User::where('email',$fields['email'])->first();
+        if(!$user){
+            return response([
+                'message'=>'Utilisateur inexistant',
+                'status' => 401,
+            ],401);
+        }
+            
+       
+
+        if($user->role_id=="1" && $fields['role']=="1"){
+            if(Hash::check($fields['password'],$user->password)){
+                $token=$user->createToken('myapptoken')->plainTextToken;
+                $response=[
+                    'user'=>$user,
+                    'token'=>$token,
+                    'message' => 'Connexion reussie',
+                    'status'=> 201,
+                ];
+
+                return response($response,201);
+            }else{
+                $response=[
+                    'message'=>'mot de passe incorrect',
+                    'status'=>'401'
+                ];
+                return response($response,401);
+            }
+        }elseif ($user->role_id=="1" && $fields['role']!=1){
+            return response('Compte client',201);
+        }
+        if($user->role_id=="2" && $fields['role']=="2"){
+            if(Hash::check($fields['password'],$user->password)){
+                $token=$user->createToken('myapptoken')->plainTextToken;
+                $response=[
+                    'user'=>$user,
+                    'token'=>$token,
+                    'message' => 'Connexion reussie',
+                    'status'=> 201,
+                ];
+
+                return response($response,201);
+            }else{
+                $response=[
+                    'message'=>'mot de passe incorrect',
+                    'status'=>'401'
+                ];
+                return response($response,401);
+            }
+        }else{
+            return response('Vous êtes en mode client',201);
+        }
+        
+
+
 
         //check password
-        if(!$user || !Hash::check($fields['password'],$user->password)){
+        /*if(!$user || !Hash::check($fields['password'],$user->password)){
             return response([
                 'message'=>'Utilisateur inexistant',
                 'status' => 401,
@@ -37,7 +92,7 @@ class UserController extends Controller
             'status'=> 201,
         ];
 
-        return response($response,201);
+        return response($response,201);*/
 
     }
 
@@ -45,13 +100,15 @@ class UserController extends Controller
         $fields=$request->validate([
             'name'=>'required|string',
             'email'=>'required|string|unique:users,email',
-            'password'=>'required|string|confirmed'
+            'password'=>'required|string|confirmed',
+            'role_id'=>'required'
         ]);
 
         $user=User::create([
             'name'=>$fields['name'],
             'email'=>$fields['email'],
-            'password'=>Hash::make($fields['password'])
+            'password'=>Hash::make($fields['password']),
+            'role_id'=>$fields['role_id']
 
         ]);
 
